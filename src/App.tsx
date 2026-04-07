6e5aa2821d34de181c6bc911cfe6c6fcd45883c5
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2026-04-07 18:46:13 +0530

    feat: add signup page UI (#4)
    * feat: added signup page and components
    
    * refactor: modularized left panel and removed ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./configs";
 import { Role } from "./types";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
 
 function App() {
   const [roles, setRoles] = useState<Role[]>([]);
@@ -27,16 +30,25 @@ function App() {
 
   if (loading) return <div>Loading roles...</div>;
   if (error) return <div>Error: {error}</div>;
-
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Roles</h1>
-      <ul>
-        {roles.map((role) => (
-          <li key={role.id}>{role.name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+      );
+      <div style={{ padding: "2rem" }}>
+        <h1>Roles</h1>
+        <ul>
+          {roles.map((role) => (
+            <li key={role.id}>{role.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 

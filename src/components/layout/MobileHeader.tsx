ec64eabc637ede09a40075a812c41bf184c1eeb9
Devangvarshney <149258579+Devangvarshney@users.noreply.github.com>
2026-04-10 22:47:13 +0530

    Improved Error UI and layout (#12)
    * Improved Error UI and layout
    
    * remove: MobileBottomNav component
    
    * remove:optform to make it modular
    
    * remove the mobilenav
    
    * updated AppLayout

---
+++ src/components/layout/MobileHeader.tsx
@@ -0,0 +1,12 @@
+
+import { ArrowLeft, MoreVertical } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
+export const MobileHeader = ({ title = "System Status" }: { title?: string }) => {
+  const navigate = useNavigate();
+
+  return (
+   <>
+   </>
+  );
+};
--- src/pages/auth/OTP.tsx
+++ src/pages/auth/OTP.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+
+import LeftPanel from "../../components/widgets/LeftPanel";
+import OTPForm from "../../components/widgets/OTPForm";
 
 const OTP = () => {
   return (
-    <div>
-      otp screen
+    <div className="flex flex-col lg:flex-row min-h-screen lg:h-screen w-full bg-[#f8fafc] lg:bg-white antialiased selection:bg-blue-200">
+
+      {/* LEFT SIDE (DESKTOP) / TOP (MOBILE) */}
+      <div className="w-full lg:w-1/2 flex-shrink-0 lg:h-full lg:overflow-y-auto">
+        <LeftPanel />
+      </div>
+
+      {/* RIGHT SIDE (FORM) */}
+      <div className="flex w-full lg:w-1/2 flex-grow lg:h-full lg:overflow-y-auto bg-[#f8fafc]">
+         <OTPForm />
+      </div>
+
     </div>
   )
 }
 
-export default OTP
+export default OTP;

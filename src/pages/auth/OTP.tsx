084df3fc159f9c27867ed37c9a6930ad052e89b1
Devangvarshney <devangvarshney43@gmail.com>
2026-04-10 22:40:04 +0530

    updated signup form changes

--- src/pages/auth/OTP.tsx
+++ src/pages/auth/OTP.tsx
@@ -4,9 +4,9 @@ import OTPForm from "../../components/widgets/OTPForm";
 const OTP = () => {
   return (
     <div className="flex min-h-screen lg:h-screen w-full items-center justify-center bg-[#f8fafc] lg:bg-white antialiased selection:bg-blue-200">
-      <div className="w-full flex-grow lg:h-full lg:overflow-y-auto bg-[#f8fafc] flex items-center justify-center">
+     
          <OTPForm />
-      </div>
+     
     </div>
   )
 }

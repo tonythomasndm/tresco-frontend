a7f4c20e8fba2a124b4795f02888d6790aa44a05
Devangvarshney <devangvarshney43@gmail.com>
2026-04-10 22:00:08 +0530

    otp auth feature added

--- src/pages/auth/OTP.tsx
+++ src/pages/auth/OTP.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import OTPForm from "../../components/widgets/OTPForm";
 
 const OTP = () => {
   return (
-    <div>
-      otp screen
+    <div className="flex min-h-screen lg:h-screen w-full items-center justify-center bg-[#f8fafc] lg:bg-white antialiased selection:bg-blue-200">
+      <div className="w-full flex-grow lg:h-full lg:overflow-y-auto bg-[#f8fafc] flex items-center justify-center">
+         <OTPForm />
+      </div>
     </div>
   )
 }
 
-export default OTP
+export default OTP;
